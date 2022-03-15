32896181de402353572bf4e6c3182f5a6468a282
tnsilva <tnsilva175@gmail.com>
2022-03-15 13:02:56 -0300

    feat: adicionado o tema claro e provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/home" element={<p>Home</p>} />
+      <Route
+        path="/home"
+        element={
+          <Button variant="contained" color="primary">
+            Home
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   );
